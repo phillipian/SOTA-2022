70851173e884ab6808fcb83c92d65e78c04bd061
clairebookworm <clara32356@gmail.com>
2022-05-19 13:57:40 -0400

    edits

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-
-// required for graphs to look okay -- specify at minimum a slug and array of colors
 sota.sotaConfig.sections = [
-    { "slug": "general", "name": "General", "colors": ["#235085", "#1065a1", "#0b88c0", "#22a7d3", "#4ac2e5", "#8dd1e9", "#c6e6f3", "#e3f4fb"] },
-    {
-        "slug": "politics", "name": "Politics", "colors": ["#2C3278", "#5D5797", "#7D78AD", "#948EB9", "#A8A3C6", "#BFBDD5", "#ACA0C0", "#DFDBE7"],
-        "blurb": "POLITICS BLUIRB!!!! "
+    {"slug": "general", "name": "General", "colors": ["#235085","#1065a1","#0b88c0","#22a7d3","#4ac2e5","#8dd1e9","#c6e6f3","#e3f4fb"]},
+    {"slug": "politics", "name": "Politics", "colors": ["#2C3278", "#5D5797", "#7D78AD", "#948EB9", "#A8A3C6", "#BFBDD5", "#ACA0C0", "#DFDBE7"],
+      "blurb": "POLITICS"
     },
-    {
-        "slug": "wellness", "name": "Wellness", "colors": ["#69B645", "#99CB89", "#98C970", "#B1D281", "#CEDFA6", "#E3EFCE", "#BAE5AE"],
-        "blurb": "WELLNESS BLUBL!! "
+    {"slug": "campus", "name": "Campus Culture", "colors": ["#762670", "#945E98", "#A77FAD", "#BC98B9", "#D9C6D8"],
+      "blurb": "CAMPUS CULTURE"
     },
+    {"slug": "school", "name": "School", "colors": ["#812F54", "#AA7281", "#BF97A0", "#BF97A0", "#DCC5C3"],
+      "blurb" : "SCHOOL "
+    },
+    {"slug": "discipline", "name": "Discipline", "colors": ["#B53432", "#C74F50", "#D47173","#E4A8A6", "#F0D1CA"],
+      "blurb": "DISCIPLINE"
+    },
+    {"slug": "sex", "name": "Sex", "colors": ["#D5127D", "#DF5694", "#E680A9", "#ECA8C1", "#F5BAD1", "#F8D3DD", "#fbe7ec"],
+      "blurb": "SEX "
+    },
+    {"slug": "drugs", "name": "Drugs & Alcohol", "colors": ["#E77929", "#EFA05E", "#F3B572", "#F6CC99", "#FAE3C4"],
+      "blurb": "DRUGS AND ALCOJHOL!!"
+    },
+    {"slug": "wellness", "name": "Wellness", "colors": ["#69B645", "#99CB89", "#98C970", "#B1D281", "#CEDFA6","#E3EFCE", "#BAE5AE"],
+      "blurb": "WELLNESS!!! "
+    },
+    {"slug": "community", "name": "Community Diversity", "colors": ["#279680", "#29AD97", "#78C4B1", "#AEDCCA", "#C8E7DD"],
+      "blurb": "COMMUNITY DIVERSITY BLURB "
+    },
+    {"slug": "senior", "name": "Senior Sota", "colors": ["#235085","#1065a1","#0b88c0","#22a7d3","#4ac2e5", "#7CB7D1", "#8dd1e9","#c6e6f3","#e3f4fb"]},
 ]
 
-// optional styling configuration
-sota.sotaConfig.numberFont = "DM Sans"; // optional -- defaults to Montserrat
-sota.sotaConfig.labelFont = "Crimson Text"; // optional -- defaults to Lora
-
-// required for graphs to look okay -- call functions to inject color, style CSS based on sotaConfig
 sota.setColors(sota.sotaConfig);
 sota.setStyles(sota.sotaConfig);
 
-// required only if you want to generate default layout containers. Can also specify directly through HTML and use selectors for graphs
-// sota.createSections(sota.sotaConfig);
+
+// required only if you want to generate default layout containers. Can also specify directly through HTML and useselectors for graphs
+sota.createSections(sota.sotaConfig);
 
 // optional, render navbar based on sotaConfig.sections
 // make sure you call createSections first!
-sota.sotaNavbar(sota.sotaConfig, "claire");
+sota.sotaNavbar(sota.sotaConfig, "", "assets/graphics/nameplate.png", false, "#intro");
 
 // render graphs inside window.onload so they will be able to find parent containers dynamically created above
 window.onload = () => {
@@ -94,9 +105,7 @@ window.onload = () => {
     //     dataFile: "data/gallery-customBar", shapeFile: "shapes/fish"
     // })
 
-
-
-    // ----- WELLENESS!!!
+    // ----- WELLNESS!!!
 
     sota.columnChart({
         section: "wellness",
